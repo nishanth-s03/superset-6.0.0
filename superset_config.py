bb2245e1687b259935d8cbea165680bfbd35e474
Nishanth_S <82016416+nishanth-s03@users.noreply.github.com>
2026-04-02 17:08:58 +0530

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")
+
+if db_url:
+    db_url = db_url.replace("postgres://", "postgresql+psycopg://")
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg://")
+
+SQLALCHEMY_DATABASE_URI = db_url
 SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY", "change-this-secret-key")
 WTF_CSRF_ENABLED = True
