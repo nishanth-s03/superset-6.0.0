8af9ebd5e206f49641a48c76ba29c3504239f760
Nishanth_S <82016416+nishanth-s03@users.noreply.github.com>
2026-04-02 17:16:16 +0530

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -3,8 +3,7 @@ import os
 db_url = os.getenv("DATABASE_URL")
 
 if db_url:
-    db_url = db_url.replace("postgres://", "postgresql+psycopg://")
-    db_url = db_url.replace("postgresql://", "postgresql+psycopg://")
+    db_url = db_url.replace("postgres://", "postgresql://")
 
 SQLALCHEMY_DATABASE_URI = db_url
 SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY", "change-this-secret-key")
